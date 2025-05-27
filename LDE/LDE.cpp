5ae0b005ac226a94ab4a8186f2d2002164196198
alomyr <matheuscastro11213@gmail.com>
2025-05-27 09:58:14 -0300

    Adicionar nós à função build123 para construir a lista duplamente encadeada

--- LDE/LDE.cpp
+++ LDE/LDE.cpp
@@ -8,6 +8,13 @@ Node* build123(){
     h->prev = nullptr;
 
     Node* n1= new Node();
-    
+    h->next=n1;
+    n1->prev=h;
+    n1->data =2;
+
+    Node* n2= new Node();
+    n1->next=n2;
+    n2->prev=n1;
+    n2->data =6;
 
 }
